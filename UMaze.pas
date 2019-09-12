8459d445f3b665607f043364847ee1fd8d893660
Lo67 <41506304+Lo67@users.noreply.github.com>
2019-09-12 11:14:04 +0300

    Maze generate work correct

--- UMaze.pas
+++ UMaze.pas
@@ -4,8 +4,9 @@ interface
 
 uses
   UStack;
+
 const
-  SizeOfMaze = 22;
+  SizeOfMaze = 30;
 
 type
   TMass = array [0..SizeOfMaze, 0..SizeOfMaze] of Char;
@@ -13,102 +14,106 @@ type
     fMazeMatrix: TMass;
     fStartPoint: TCoordinate;
     fFinishPoint: TCoordinate;
+    fCurrentPoint: TCoordinate;
     fUnvisitedCount: Integer;
   public
-    procedure StartMatrix();
     constructor CreateMaze();
-    procedure MakeVisited (aCurrentPoint: TCoordinate);
-    function RandomDirection(aCurrentPoint: TCoordinate): Integer;
-    procedure DestroyWall(aCurrentPoint: TCoordinate; aDirection: Integer);
+  private
+    procedure GenStartMatrix();
+    procedure GenMakeVisited ();
+    function GenRandomDirection(): Integer;
+    procedure GenDestroyWall(aDirection: Integer);
   end;
 
 implementation
 
-procedure TMaze.StartMatrix();
+procedure TMaze.GenStartMatrix();
 var
   i, j: Integer;
 begin
   for i := 0 to SizeOfMaze do
     for j := 0 to SizeOfMaze do
       if (i mod 2 <> 0) and (j mod 2 <> 0) and (i < SizeOfMaze) and (j < SizeOfMaze) then
-        fMazeMatrix[i,j] := ' '
+        fMazeMatrix[i,j] := '0'
       else
-        fMazeMatrix[i,j] := '1';
-  fMazeMatrix[21,0] := ' ';
-  with fStartPoint do
-  begin
-    X := 1;
-    Y := 1;
-  end;
-  with fFinishPoint do  //В конце присвоить финальной клетке текущую клетку
-  begin
-    X := 21;
-    Y := 21;
-  end;
+        fMazeMatrix[i,j] := '#';
 end;
 
 constructor TMaze.CreateMaze();   //Main procedure
 var
-  CurrentPoint: TCoordinate;
   Stack: TMyStack;
+  Direction: Integer;
 begin
-  Self.fUnvisitedCount := sqr(SizeOfMaze div 2);
-  Self.StartMatrix;
+  //Preparation
+  with Self do
+  begin
+    fUnvisitedCount := sqr(SizeOfMaze div 2);
+    GenStartMatrix();
+    fStartPoint.X := 1;
+    fStartPoint.Y := 1;
+    fCurrentPoint := fStartPoint;
+    GenMakeVisited();
+  end;
 
-  CurrentPoint := fStartPoint;
-
-  Self.MakeVisited(CurrentPoint);
-
-  Stack.Create;
-
+  Stack := TMyStack.Create;
+  //Generation of Maze
   while fUnvisitedCount > 0 do //Проверить условие
   begin
-    if self.RandomDirection(CurrentPoint) <> -1 then
+    if Self.GenRandomDirection <> -1 then
     begin
-      Stack.Add(CurrentPoint);
-      Self.DestroyWall(CurrentPoint,Self.RandomDirection(CurrentPoint));
+      Stack.Add(Self.fCurrentPoint);
+      Direction:= Self.GenRandomDirection;
+      Self.GenDestroyWall(Direction);
     end
     else
       if not Stack.isEmpty then
       begin
-        CurrentPoint := Stack.Take;
+        Self.fCurrentPoint := Stack.Take;
       end;
+  end;
+  //Mark Start and Finish
+  with Self do
+  begin
+    fMazeMatrix[fStartPoint.X, fStartPoint.Y] := '0';
+    fFinishPoint.X := SizeOfMaze - 1;
+    fFinishPoint.Y := SizeOfMaze - 1;
+    fMazeMatrix[fFinishPoint.X, fFinishPoint.Y] := '1';
   end;
 end;
 
-procedure TMaze.MakeVisited (aCurrentPoint: TCoordinate);
+procedure TMaze.GenMakeVisited ();
 begin
-  with aCurrentPoint do
-    Self.fMazeMatrix[X,Y] := '0';
+  with Self.fCurrentPoint do
+    Self.fMazeMatrix[X,Y] := ' ';
   Dec(Self.fUnvisitedCount);
 end;
 
-function TMaze.RandomDirection(aCurrentPoint: TCoordinate): Integer;
+function TMaze.GenRandomDirection(): Integer;
 var
   RandomMass: array [0..3] of Integer;
   Count: Integer;
 begin
   Count := 0;
   //Search true direction
-  if (aCurrentPoint.X - 2 > 0) and (Self.fMazeMatrix[aCurrentPoint.X - 2,aCurrentPoint.Y] <> '0') then
+  if (Self.fCurrentPoint.X - 2 > 0) and (Self.fMazeMatrix[Self.fCurrentPoint.X - 2,Self.fCurrentPoint.Y] <> ' ') then
   begin
     RandomMass[Count] := 0;
     Inc(Count);
   end;
 
-  if (aCurrentPoint.Y + 2 < SizeOfMaze) and (Self.fMazeMatrix[aCurrentPoint.X,aCurrentPoint.Y + 2] <> '0') then
+  if (Self.fCurrentPoint.Y + 2 < SizeOfMaze) and (Self.fMazeMatrix[Self.fCurrentPoint.X,Self.fCurrentPoint.Y + 2] <> ' ') then
   begin
     RandomMass[Count] := 1;
     Inc(Count);
   end;
 
-  if (aCurrentPoint.X + 2 < SizeOfMaze) and (Self.fMazeMatrix[aCurrentPoint.X + 2,aCurrentPoint.Y] <> '0') then
+  if (Self.fCurrentPoint.X + 2 < SizeOfMaze) and (Self.fMazeMatrix[Self.fCurrentPoint.X + 2,Self.fCurrentPoint.Y] <> ' ') then
   begin
     RandomMass[Count] := 2;
     Inc(Count);
   end;
 
-  if (aCurrentPoint.Y - 2 > 0) and (Self.fMazeMatrix[aCurrentPoint.X,aCurrentPoint.Y - 2] <> '0') then
+  if (Self.fCurrentPoint.Y - 2 > 0) and (Self.fMazeMatrix[Self.fCurrentPoint.X,Self.fCurrentPoint.Y - 2] <> ' ') then
   begin
     RandomMass[Count] := 3;
     Inc(Count);
@@ -117,37 +122,37 @@ begin
   //Get random Direction
   Randomize;
   if Count <> 0 then
-    Result := Random(111) mod Count
+    Result := RandomMass[Random(111) mod Count]
   else
     Result := -1;
 end;
 
-procedure TMaze.DestroyWall(aCurrentPoint: TCoordinate; aDirection: Integer);
+procedure TMaze.GenDestroyWall(aDirection: Integer);
 begin
   case aDirection of
     0:
     begin
-      Self.fMazeMatrix[aCurrentPoint.X - 1, aCurrentPoint.Y] := '0';
-      aCurrentPoint.X := aCurrentPoint.X - 2;
-      Self.MakeVisited(aCurrentPoint);
+      Self.fMazeMatrix[Self.fCurrentPoint.X - 1, Self.fCurrentPoint.Y] := ' ';
+      Self.fCurrentPoint.X := Self.fCurrentPoint.X - 2;
+      Self.GenMakeVisited;
     end;
     1:
     begin
-      Self.fMazeMatrix[aCurrentPoint.X, aCurrentPoint.Y + 1] := '0';
-      aCurrentPoint.Y := aCurrentPoint.Y + 2;
-      Self.MakeVisited(aCurrentPoint);
+      Self.fMazeMatrix[Self.fCurrentPoint.X, Self.fCurrentPoint.Y + 1] := ' ';
+      Self.fCurrentPoint.Y := Self.fCurrentPoint.Y + 2;
+      Self.GenMakeVisited;
     end;
     2:
     begin
-      Self.fMazeMatrix[aCurrentPoint.X + 1, aCurrentPoint.Y] := '0';
-      aCurrentPoint.X := aCurrentPoint.X + 2;
-      Self.MakeVisited(aCurrentPoint);
+      Self.fMazeMatrix[Self.fCurrentPoint.X + 1, Self.fCurrentPoint.Y] := ' ';
+      Self.fCurrentPoint.X := Self.fCurrentPoint.X + 2;
+      Self.GenMakeVisited;
     end;
     3:
     begin
-      Self.fMazeMatrix[aCurrentPoint.X, aCurrentPoint.Y - 1] := '0';
-      aCurrentPoint.Y := aCurrentPoint.Y - 2;
-      Self.MakeVisited(aCurrentPoint);
+      Self.fMazeMatrix[Self.fCurrentPoint.X, Self.fCurrentPoint.Y - 1] := ' ';
+      Self.fCurrentPoint.Y := Self.fCurrentPoint.Y - 2;
+      Self.GenMakeVisited;
     end;
   end;
 end;
